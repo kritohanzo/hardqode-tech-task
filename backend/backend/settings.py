b5c183066c9ab3413a9d0b64ed2b05cff98ca999
Vagif Aliev <kritohanzo0@gmail.com>
2023-09-21 23:03:24 +0500

    add user model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users.apps.UsersConfig",
+    "education.apps.EducationConfig",
+    "core.apps.CoreConfig",
+    "api.apps.ApiConfig",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"
--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    username = models.CharField(
+        verbose_name="Логин", max_length=16, unique=True
+    )
+    email = models.EmailField(
+        verbose_name="Почта", max_length=256, unique=True
+    )
+
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["username", "email"], name="unique appversion"
+            )
+        ]
