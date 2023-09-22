082458bb8c6da353be4fe01b2fe6c6b2f89983d7
Vagif. A <kritohanzo0@gmail.com>
2023-09-22 14:28:59 +0500

    add user enum config

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,22 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 
+from core.enums import UserEnumConfig
+
 
 class User(AbstractBaseUser):
     username = models.CharField(
-        verbose_name="Логин", max_length=16, unique=True
+        verbose_name=UserEnumConfig.USERNAME_VERBOSE_NAME.value, max_length=UserEnumConfig.USERNAME_MAX_LENGTH.value, unique=True
     )
     email = models.EmailField(
-        verbose_name="Почта", max_length=256, unique=True
+        verbose_name=UserEnumConfig.EMAIL_VERBOSE_NAME.value, max_length=UserEnumConfig.EMAIL_MAX_LENGTH.value, unique=True
     )
 
-    EMAIL_FIELD = "email"
-    USERNAME_FIELD = "username"
-    REQUIRED_FIELDS = ["email"]
+    EMAIL_FIELD = UserEnumConfig.EMAIL_FIELD.value
+    USERNAME_FIELD = UserEnumConfig.USERNAME_FIELD.value
+    REQUIRED_FIELDS = UserEnumConfig.REQUIRED_FIELDS.value
 
     class Meta:
-        verbose_name = "Пользователь"
-        verbose_name_plural = "Пользователи"
+        verbose_name = UserEnumConfig.MODEL_VERBOSE_NAME.value
+        verbose_name_plural = UserEnumConfig.MODEL_VERBOSE_NAME_PLURAL.value
         constraints = [
             models.UniqueConstraint(
                 fields=["username", "email"], name="unique appversion"
