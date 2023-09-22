3f695fdb31bfacb21c539adaef670c0aa368f151
Vagif Aliev <kritohanzo0@gmail.com>
2023-09-22 23:29:33 +0500

    add education models, push for pull tomorrow

--- backend/core/enums.py
+++ backend/core/enums.py
@@ -3,9 +3,33 @@ from enum import Enum
 
 class UserModelConfig(Enum):
     USERNAME_MAX_LENGTH = 16
-    EMAIL_MAX_LENGTH = 256
     USERNAME_VERBOSE_NAME = "Логин"
+    EMAIL_MAX_LENGTH = 256
     EMAIL_VERBOSE_NAME = "Почта"
     IS_STAFF_VERBOSE_NAME = "Персонал"
     MODEL_VERBOSE_NAME = "Пользователь"
     MODEL_VERBOSE_NAME_PLURAL = "Пользователи"
+
+class PruductModelConfig(Enum):
+    NAME_MAX_LENGTH = 256
+    NAME_VERBOSE_NAME = "Название"
+    OWNER_VERBOSE_NAME = "Владалец"
+    OWNER_RELATED_NAME = "owner_products"
+    MODEL_VERBOSE_NAME = "Продукт"
+    MODEL_VERBOSE_NAME_PLURAL = "Продукты"
+
+class LessonModelConfig(Enum):
+    NAME_MAX_LENGTH = 256
+    NAME_VERBOSE_NAME = "Название"
+    LINK_TO_VIDEO_VERBOSE_NAME = "Ссылка на видео"
+    VIEWING_DURATION_VERBOSE_NAME = "Длительность просмотра"
+    MODEL_VERBOSE_NAME = "Урок"
+    MODEL_VERBOSE_NAME_PLURAL = "Уроки"
+
+class UserProductModelConfig(Enum):
+    USER_VERBOSE_NAME = "Пользователь"
+    USER_RELATED_NAME = "user_products"
+    PRODUCT_VERBOSE_NAME = "Продукт"
+    PRODUCT_RELATED_NAME = "product_users"
+    MODEL_VERBOSE_NAME = "Участие пользователя в продукте"
+    MODEL_VERBOSE_NAME_PLURAL = "Участия пользователей в продукте"
--- backend/education/models.py
+++ backend/education/models.py
@@ -1,4 +1,75 @@
 from django.db import models
+from users.models import User
+from core.enums import (
+    PruductModelConfig,
+    LessonModelConfig,
+    UserProductModelConfig,
+)
 
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(
+        verbose_name=PruductModelConfig.NAME_VERBOSE_NAME.value,
+        max_length=PruductModelConfig.NAME_MAX_LENGTH.value,
+    )
+    owner = models.ForeignKey(
+        to=User,
+        verbose_name=PruductModelConfig.OWNER_VERBOSE_NAME.value,
+        related_name=PruductModelConfig.OWNER_RELATED_NAME.value,
+        on_delete=models.SET_NULL,
+    )
+
+    class Meta:
+        verbose_name = PruductModelConfig.MODEL_VERBOSE_NAME.value
+        verbose_name_plural = (
+            PruductModelConfig.MODEL_VERBOSE_NAME_PLURAL.value
+        )
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    name = models.CharField(
+        verbose_name=LessonModelConfig.NAME_VERBOSE_NAME.value,
+        max_length=LessonModelConfig.NAME_MAX_LENGTH.value,
+    )
+    link_to_video = models.URLField(
+        verbose_name=LessonModelConfig.LINK_TO_VIDEO_VERBOSE_NAME.value
+    )
+    viewing_duration = models.PositiveIntegerField(
+        verbose_name=LessonModelConfig.VIEWING_DURATION_VERBOSE_NAME.value
+    )
+
+    class Meta:
+        verbose_name = LessonModelConfig.MODEL_VERBOSE_NAME.value
+        verbose_name_plural = LessonModelConfig.MODEL_VERBOSE_NAME_PLURAL.value
+
+    def __str__(self):
+        return self.name
+
+
+class ProductLesson(models.Model):
+    ...
+
+
+class UserProduct(models.Model):
+    user = models.ForeignKey(
+        to=User,
+        verbose_name=UserProductModelConfig.USER_VERBOSE_NAME.value,
+        related_name=UserProductModelConfig.USER_RELATED_NAME.value,
+    )
+    product = models.ForeignKey(
+        to=Product,
+        verbose_name=UserProductModelConfig.PRODUCT_VERBOSE_NAME.value,
+        related_name=UserProductModelConfig.PRODUCT_RELATED_NAME.value,
+    )
+
+    class Meta:
+        verbose_name = UserProductModelConfig.MODEL_VERBOSE_NAME.value
+        verbose_name_plural = (
+            UserProductModelConfig.MODEL_VERBOSE_NAME_PLURAL.value
+        )
+
+    def __str__(self):
+        return f"{self.user} - участник продукта {self.product}"
